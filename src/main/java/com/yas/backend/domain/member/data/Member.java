e6e6bee063141e3e372fb05d585e09a088fd25f3
hgs6424 <hgs6424@naver.com>
2022-03-11 20:59:02 +0900

    use birth/careerStartAt & boolean variable name change

--- src/main/java/com/yas/backend/domain/member/data/Member.java
+++ src/main/java/com/yas/backend/domain/member/data/Member.java
@@ -3,13 +3,16 @@ package com.yas.backend.domain.member.data;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class Member {
     private String email;
     private String password;
     private String nickname;
-    private Integer birthYear;
-    private Boolean showBirthYear;
-    private Integer careerYear;
+    private LocalDate birth;
+    private Boolean showsBirth;
+    private LocalDate careerStartAt;
 }
