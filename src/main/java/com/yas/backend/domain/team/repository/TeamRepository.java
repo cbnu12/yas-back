167de144baeb959054446876d8dd9e0d281f3188
hgs6424 <hgs6424@naver.com>
2022-03-11 21:15:15 +0900

    make TeamRepository extends JpaRepository

--- src/main/java/com/yas/backend/domain/team/repository/TeamRepository.java
+++ src/main/java/com/yas/backend/domain/team/repository/TeamRepository.java
@@ -1,7 +1,9 @@
 package com.yas.backend.domain.team.repository;
 
+import com.yas.backend.domain.team.entity.TeamEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository {
+public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
 }
