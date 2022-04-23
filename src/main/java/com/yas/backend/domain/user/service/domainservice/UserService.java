e4208f828468ac944621dff3d81e843a21494f23
hgs6424 <hgs6424@naver.com>
2022-04-23 10:07:53 +0900

    fix UserService to domain service

--- src/main/java/com/yas/backend/domain/user/service/domainservice/UserService.java
+++ src/main/java/com/yas/backend/domain/user/service/domainservice/UserService.java
@@ -1,17 +1,12 @@
 package com.yas.backend.domain.user.service.domainservice;
 
 import com.yas.backend.common.entity.UserEntity;
-import com.yas.backend.common.enums.LoginResponseCode;
 import com.yas.backend.common.exception.UserNotFoundException;
-import com.yas.backend.domain.user.data.User;
 import com.yas.backend.domain.user.data.dto.UserDto;
 import com.yas.backend.domain.user.data.mapper.UserMapper;
-import com.yas.backend.domain.user.data.response.LoginResponse;
 import com.yas.backend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +14,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserDto findByEmail(UserDto userDto) {
-        UserEntity userEntity = userRepository.findByEmail(userDto.getEmail())
+    public UserDto findByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email)
                 .orElseThrow(UserNotFoundException::new);
 
         return userMapper.entityToDto(userEntity);
