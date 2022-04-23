666d6f6179b396e45ba40fb170b24942842e0e6a
hgs6424 <hgs6424@naver.com>
2022-04-23 09:48:00 +0900

    fix to have error message in UserNotFoundException

--- src/main/java/com/yas/backend/common/exception/UserNotFoundException.java
+++ src/main/java/com/yas/backend/common/exception/UserNotFoundException.java
@@ -1,4 +1,10 @@
 package com.yas.backend.common.exception;
 
+import com.yas.backend.common.enums.ErrorType;
+
 public class UserNotFoundException extends RuntimeException {
+
+    UserNotFoundException() {
+        super(ErrorType.NOT_FOUND_USER.getMessage());
+    }
 }
