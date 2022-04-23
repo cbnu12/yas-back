726601837006f4037522aeaadda971d0856166d3
hgs6424 <hgs6424@naver.com>
2022-04-23 10:07:13 +0900

    fix find a user api to use UserSearchService

--- src/main/java/com/yas/backend/domain/user/controller/UserController.java
+++ src/main/java/com/yas/backend/domain/user/controller/UserController.java
@@ -2,32 +2,31 @@ package com.yas.backend.domain.user.controller;
 
 import com.yas.backend.common.controller.BaseController;
 import com.yas.backend.domain.user.data.dto.UserDto;
+import com.yas.backend.domain.user.data.exchange.UserSearchRequest;
 import com.yas.backend.domain.user.data.mapper.UserMapper;
-import com.yas.backend.domain.user.data.response.LoginResponse;
-import com.yas.backend.domain.user.data.response.UserResponse;
-import com.yas.backend.domain.user.service.UserService;
+import com.yas.backend.domain.user.data.exchange.UserResponse;
+import com.yas.backend.domain.user.service.UserSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController extends BaseController {
-    private final UserService userService;
+    private final UserSearchService userSearchService;
     private final UserMapper userMapper;
-
-    @GetMapping("users")
-    public List<UserResponse> findAllActiveUser() {
-        return userService.findAllActiveUser().stream().map(userMapper::dtoToResponse).toList();
-    }
+//
+//    @GetMapping("users")
+//    public List<UserResponse> findAllActiveUser() {
+//        return userService.findAllActiveUser().stream().map(userMapper::dtoToResponse).toList();
+//    }
 
     @GetMapping("user")
-    public UserResponse findActiveUserByEmail(@Param("email") String email) {
-        return userMapper.dtoToResponse(userService.findUserByEmail(email));
+    public UserResponse findUserByEmail(UserSearchRequest request) {
+        return userMapper.dtoToResponse(userSearchService.findUserByEmail(request.toDto()));
     }
 }
---
+++ src/main/java/com/yas/backend/domain/user/service/UserSearchService.java
@@ -0,0 +1,21 @@
+package com.yas.backend.domain.user.service;
+
+import com.yas.backend.domain.user.data.dto.UserDto;
+import com.yas.backend.domain.user.data.mapper.UserMapper;
+import com.yas.backend.domain.user.service.domainservice.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserSearchService {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    public UserDto findUserByEmail(UserDto userDto) {
+        return userService.findByEmail(userDto.getEmail());
+    }
+}
