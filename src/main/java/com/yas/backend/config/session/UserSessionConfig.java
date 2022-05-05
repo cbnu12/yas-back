b3b9995988ffab614fa91f72b3309211f99ecd42
hgs6424 <hgs6424@naver.com>
2022-05-05 09:48:55 +0900

    fix userSession bean scope (session -> request)

--- src/main/java/com/yas/backend/config/session/UserSessionConfig.java
+++ src/main/java/com/yas/backend/config/session/UserSessionConfig.java
@@ -2,13 +2,13 @@ package com.yas.backend.config.session;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.SessionScope;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class UserSessionConfig {
 
     @Bean
-    @SessionScope
+    @RequestScope
     public UserSession userSession() {
         return new UserSession();
     }
