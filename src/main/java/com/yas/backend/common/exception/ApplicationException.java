4f3edf514c4df88bbb63d53282a395b7851e00a3
hgs6424 <hgs6424@naver.com>
2022-05-12 23:15:00 +0900

    add ApplicationException

---
+++ src/main/java/com/yas/backend/common/exception/ApplicationException.java
@@ -0,0 +1,8 @@
+package com.yas.backend.common.exception;
+
+public class ApplicationException extends RuntimeException{
+
+    public ApplicationException() {
+        super("application exception");
+    }
+}
