36e0bb120bde14b50dc3979761b9184d3e22ae8d
hgs6424 <hgs6424@naver.com>
2022-05-13 12:29:29 +0900

    fix GlobalExceptionHandler
    1. add ValidationException Handler
    2. move package

--- src/main/java/com/yas/backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yas/backend/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
-package com.yas.backend;
+package com.yas.backend.common.exception;
 
-import com.yas.backend.common.exception.ErrorResponse;
-import com.yas.backend.common.exception.YasBaseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+import javax.validation.ValidationException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,11 +21,20 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatusCode()));
     }
 
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ErrorResponse> handleException(ValidationException exception) {
+        log.info(exception.getClass().getName(), exception.getMessage());
+        log.info(Arrays.toString(exception.getStackTrace()));
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase());
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatusCode()));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception exception) {
         log.error(exception.getClass().getName(), exception.getMessage());
         String errorMessage = String.format("데이터 처리 중 오류가 발생하였습니다.%n%s", LocalDateTime.now());
-        ErrorResponse response = new ErrorResponse(500, errorMessage);
+        ErrorResponse response = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorMessage);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getHttpStatusCode()));
     }
+
 }
