4143c643fdd260c8ff26450363cb6ad099cd3fe1
hgs6424 <hgs6424@naver.com>
2022-05-22 21:21:10 +0900

    delete not using file

--- src/main/java/com/yas/backend/common/exception/ApplicationException.java
+++
@@ -1,8 +0,0 @@
-package com.yas.backend.common.exception;
-
-public class ApplicationException extends RuntimeException{
-
-    public ApplicationException() {
-        super("application exception");
-    }
-}
